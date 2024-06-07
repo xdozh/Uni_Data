af3171c8a57fbd3740782912227e0a108d26ff2a
Shourya Maithani <smaithani010@gmail.com>
2024-06-07 06:18:20 +0530

    V7

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
+
+const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+            <div className="pt-[4,75rem] lg:pt-[5.25rem] overflow-hidden">
+                <Header />
+            </div>
+
+
+            <ButtonGradient/>
+        </>
     )
 }
 export default App
