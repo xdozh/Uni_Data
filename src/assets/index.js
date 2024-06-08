bd16fc25fd23863369a2655473a22787960703f7
Shourya Maithani <smaithani010@gmail.com>
2024-06-08 23:07:15 +0530

    V10

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
-import brainwave from "./brainwave.svg";
+import resourcelib from "./resourcelib.png";
 import check from "./check.svg";
-import brainwaveSymbol from "./brainwave-symbol.svg";
-import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
+import resourcelibSymbol from "./resourcelib-symbol.png";
+import resourcelibWhiteSymbol from "./resourcelib-symbol-white.png";
 import play from "./play.svg";
 import gradient from "./gradient.png";
 import smallSphere from "./4-small.png";
@@ -76,12 +76,12 @@ import telegram from "./socials/telegram.svg";
 import twitter from "./socials/twitter.svg";
 
 export {
-  brainwave,
+  resourcelib,
   check,
   check2,
   loading1,
-  brainwaveSymbol,
-  brainwaveWhiteSymbol,
+  resourcelibSymbol,
+  resourcelibWhiteSymbol,
   play,
   gradient,
   smallSphere,
--- src/constants/index.js
+++ src/constants/index.js
@@ -56,19 +56,7 @@ export const navigation = [
         id: "3",
         title: "Roadmap",
         url: "#roadmap",
-    },
-    {
-        id: "4",
-        title: "New account",
-        url: "#signup",
-        onlyMobile: true,
-    },
-    {
-        id: "5",
-        title: "Sign in",
-        url: "#login",
-        onlyMobile: true,
-    },
+    }
 ];
 
 export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
