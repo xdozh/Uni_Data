c480b1a067ff7142b4aa9c485565540ac61dcac1
Shourya Maithani <smaithani010@gmail.com>
2024-06-08 21:24:47 +0530

    V9

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Button from "./components/Button.jsx";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import Benefits from "./components/Benefits.jsx";
+import Collaboration from "./components/Collaboration.jsx";
+import Services from "./components/Sevices.jsx";
+import Pricing from "./components/Pricing.jsx";
+import Roadmap from "./components/Roadmap.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
     return (
@@ -11,6 +16,11 @@ const App = () => {
                 <Header />
                 <Hero/>
                 <Benefits/>
+                <Collaboration/>
+                <Services/>
+                <Pricing/>
+                <Roadmap/>
+                <Footer/>
             </div>
 
             <ButtonGradient/>
