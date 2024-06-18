6605c4747c473d3be56cba3d8d1fea3bd49ab40f
Shourya Maithani <smaithani010@gmail.com>
2024-06-18 13:32:08 +0530

    V26

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
@@ -8,33 +7,8 @@ import CEN from "./components/CEN.jsx";
 import Contributions from "./components/Contributions.jsx";
 import Footer from "./components/Footer.jsx";
 
-const logPerformance = (metric) => {
-    console.log(metric);
-    // Optionally send the metric to your analytics endpoint
-    fetch('/analytics', {
-        method: 'POST',
-        body: JSON.stringify(metric),
-        headers: { 'Content-Type': 'application/json' },
-    });
-};
-
-const usePerformanceMetrics = () => {
-    useEffect(() => {
-        const observer = new PerformanceObserver((list) => {
-            list.getEntries().forEach((entry) => {
-                logPerformance(entry.toJSON());
-            });
-        });
-        observer.observe({ type: 'navigation', buffered: true });
-        observer.observe({ type: 'paint', buffered: true });
-
-        return () => observer.disconnect();
-    }, []);
-};
 
 const App = () => {
-    usePerformanceMetrics();
-
     return (
         <>
             <div className="pt-[4,75rem] lg:pt-[5.25rem] overflow-hidden">
@@ -46,9 +20,9 @@ const App = () => {
                 <Contributions/>
                 <Footer/>
             </div>
+
             <ButtonGradient/>
         </>
     )
 }
-
-export default App;
+export default App
