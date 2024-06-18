fa8a557a97180e152ca7b7479e9418d6bbf470ed
Shourya Maithani <smaithani010@gmail.com>
2024-06-18 13:15:26 +0530

    V24

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter as Router} from "react-router-dom";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
         <App />
+        <SpeedInsights />
     </Router>
   </React.StrictMode>,
 )
